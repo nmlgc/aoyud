c36214c18a60b01ebbc7c0e8a28d085cdee9c389
nmlgc <nmlgc@nmlgc.net>
2015-01-10 22:51:12 +0100

    [Parser] Use individual functions for each instruction or directive
    While it would have been possible to use a reflection-based solution in order
    to avoid the map, that 1) would have required ugly workarounds for special
    characters and 2) introduced even more duplication for cases where one
    function should handle multiple instructions.

--- aoyud.go
+++ aoyud.go
@@ -304,6 +304,36 @@ type parser struct {
 	procName  string
 }
 
+// parseFn represents a function handling a certain instruction or directive.
+type parseFn func(p *parser, itemNum int, i *item)
+
+func (p *parser) parsePROC(itemNum int, i *item) {
+	if p.procNest == 0 {
+		p.procName = p.symLast
+		p.procStart = itemNum
+	} else {
+		log.Printf("ignoring nested procedure %s\n", p.symLast)
+	}
+	p.procNest++
+}
+
+func (p *parser) parseENDP(itemNum int, i *item) {
+	if p.procNest == 0 {
+		log.Printf("ignoring procedure %s without a PROC directive\n", p.symLast)
+	} else if p.procNest == 1 {
+		log.Printf(
+			"found procedure %s ranging from lex items #%d-#%d\n",
+			p.procName, p.procStart, itemNum,
+		)
+	}
+	p.procNest--
+}
+
+var parseFns = map[string]parseFn{
+	"PROC": (*parser).parsePROC,
+	"ENDP": (*parser).parseENDP,
+}
+
 func (p *parser) eval(i *item) {
 	itemNum := len(p.instructions)
 	valString := string(i.val)
@@ -313,24 +343,9 @@ func (p *parser) eval(i *item) {
 	case itemSymbol:
 		p.symLast = valString
 	case itemInstruction:
-		if strings.EqualFold(valString, "PROC") {
-			if p.procNest == 0 {
-				p.procName = p.symLast
-				p.procStart = itemNum
-			} else {
-				log.Printf("ignoring nested procedure %s\n", p.symLast)
-			}
-			p.procNest++
-		} else if strings.EqualFold(valString, "ENDP") {
-			if p.procNest == 0 {
-				log.Printf("ignoring procedure %s without a PROC directive\n", p.symLast)
-			} else if p.procNest == 1 {
-				log.Printf(
-					"found procedure %s ranging from lex items #%d-#%d\n",
-					p.procName, p.procStart, itemNum,
-				)
-			}
-			p.procNest--
+		valUpper := strings.ToUpper(valString)
+		if insFunc := parseFns[valUpper]; insFunc != nil {
+			insFunc(p, itemNum, i)
 		}
 	}
 }
